1e70ec0086f8613e40dd2262c6d364259695c871
ssibyl <vairapaulina@gmail.com>
2023-12-21 23:53:14 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <driver/gpio.h>
+
+extern "C" void app_main();
+
+void app_main() 
+{
+    // setup
+
+    // loop
+    while(1)
+    {
+        
+    }
+}
